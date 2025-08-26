375c64af8bfe6359cbdbf39c7653eaca01032d41
Chip Bell <chipbell4@gmail.com>
2025-08-25 23:47:26 -0400

    Refactoring out to a module

---
+++ apple.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+def asrun(ascript):
+    osa = subprocess.run(
+        ["/usr/bin/osascript", "-"], input=ascript, text=True, capture_output=True
+    )
+    if osa.returncode == 0:
+        return osa.stdout.rstrip()
+    else:
+        raise ChildProcessError(f"AppleScript: {osa.stderr.rstrip()}")
+
+
+def add_item(list_name: str, item: str, note: str = ""):
+    with open("add-item.scpt", "r") as f:
+        template = f.read()
+
+    script = (
+        template.replace("%LIST%", list_name)
+        .replace("%ITEM%", item)
+        .replace("%NOTE%", note)
+    )
+
+    return asrun(script)
